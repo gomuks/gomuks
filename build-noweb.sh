753e2fe47cb78ceafb32fc0d60273d60fbcb0734
Tulir Asokan <tulir@maunium.net>
2026-06-01 01:02:17 +0300

    build: use /bin/sh instead of bash

--- build-noweb.sh
+++ build-noweb.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh
 mkdir -p web/dist/
-if [[ -z "$(ls -A web/dist/)" ]]; then
+if [ -z "$(ls -A web/dist/)" ]; then
 	touch web/dist/empty
 fi
 BINARY_NAME=gomuks MAU_VERSION_PACKAGE=go.mau.fi/gomuks/version go tool maubuild -tags "sqlite_fts5 $GO_BUILD_TAGS" "$@"
--- build-terminal.sh
+++ build-terminal.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env bash
+#!/bin/sh
 BINARY_NAME=gomuks-terminal MAU_VERSION_PACKAGE=go.mau.fi/gomuks/version go tool maubuild -tags "$GO_BUILD_TAGS" "$@"
