d749519c725434d664419bd3fc972a159a82fd5f
Henry Hiles <henry@henryhiles.com>
2026-09-20 11:37:59 -0400

    ffi: remove `root` param from `GomuksInit`, add `GomuksSetEnv` (#756)

--- pkg/gomuks/gomuks.go
+++ pkg/gomuks/gomuks.go
@@ -52,8 +52,6 @@ type Gomuks struct {
 	Server *http.Server
 	Client *hicli.HiClient
 
-	RootOverride string
-
 	ConfigDir string
 	DataDir   string
 	CacheDir  string
@@ -111,7 +109,7 @@ func (gmx *Gomuks) InitDirectories() {
 	// We need 4 directories: config, data, cache, logs
 	//
 	// 1. If GOMUKS_*_HOME is set, that value is used as the directory.
-	// 2. If GOMUKS_ROOT or the root argument is set, all directories are created under that.
+	// 2. If GOMUKS_ROOT is set, all directories are created under that.
 	// 3. Use system-specific defaults as below
 	//
 	// *nix:
@@ -129,7 +127,7 @@ func (gmx *Gomuks) InitDirectories() {
 	// - Config and Data: $HOME/Library/Application Support/gomuks
 	// - Cache: $HOME/Library/Caches/gomuks
 	// - Logs: $HOME/Library/Logs/gomuks
-	gomuksRoot := cmp.Or(gmx.RootOverride, os.Getenv("GOMUKS_ROOT"))
+	gomuksRoot := os.Getenv("GOMUKS_ROOT")
 	gmx.CacheDir = os.Getenv("GOMUKS_CACHE_HOME")
 	gmx.ConfigDir = os.Getenv("GOMUKS_CONFIG_HOME")
 	gmx.DataDir = os.Getenv("GOMUKS_DATA_HOME")
